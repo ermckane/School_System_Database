ae853bc145de1d535168fd068922f8000b4c3f51
Emry McKane <103011352+ermckane@users.noreply.github.com>
2022-06-08 09:04:43 -0400

    Update Populating_Student_Info.sql

--- Populating_Student_Info.sql
+++ Populating_Student_Info.sql
@@ -1,3 +1,4 @@
+--Need to add comments and add original data files
 
 Select top 10000 name into #Name_Last
 from sql_practice.dbo.last_names
@@ -60,16 +61,16 @@ CREATE TABLE #Student_Name
 
 WITH Firstlast 
 AS
-	(
-	 SELECT First_name, Last_name, ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_Fl
-	 FROM #First_Last_Name
-	) 
+  (
+   SELECT First_name, Last_name, ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_Fl
+   FROM #First_Last_Name 
+  ) 
 ,Middle 
 AS
-	(
-	 SELECT Middle_Name, ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_M
-	 FROM #Name_Middle
-	)
+  (
+   SELECT Middle_Name, ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_M
+   FROM #Name_Middle
+  )
 INSERT INTO #Student_Name (First_Name, Middle_Name, Last_Name)
 SELECT Fl.First_Name, M.Middle_Name as Middle_Name, Fl.Last_Name 
 FROM Firstlast AS Fl
@@ -84,71 +85,71 @@ INNER JOIN Middle AS M
 
 
 WITH Uncleaned_Code 
- AS
+AS
    (
     SELECT  
-		  ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Student_Code_Unfixed
-		 ,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_C
-	FROM #Student_Name
+	 ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Student_Code_Unfixed
+	,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_C
+    FROM #Student_Name
    )
 ,Cleaned_Code
- AS
+AS
    (
     SELECT
-	      Rank_C
-	     ,CASE
-		     WHEN LEN(Student_Code_Unfixed) > 9 THEN Student_Code_Unfixed / 4
-		     WHEN LEN(Student_Code_Unfixed) = 8 THEN Student_Code_Unfixed * 10
-		     WHEN LEN(Student_Code_Unfixed) = 7 THEN Student_Code_Unfixed * 100
+	 Rank_C
+	,CASE
+		WHEN LEN(Student_Code_Unfixed) > 9 THEN Student_Code_Unfixed / 4
+		WHEN LEN(Student_Code_Unfixed) = 8 THEN Student_Code_Unfixed * 10
+		WHEN LEN(Student_Code_Unfixed) = 7 THEN Student_Code_Unfixed * 100
 		     ELSE Student_Code_Unfixed
-	      END as Student_Code
-     FROM Uncleaned_Code
+	 END AS Student_Code
+    FROM Uncleaned_Code
    )
 ,Names
- AS
+AS
    (
     SELECT
-		  First_Name
-		 ,Middle_Name
-		 ,Last_name
-		 ,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_N
-	FROM #Student_Name
+	 First_Name
+        ,Middle_Name
+        ,Last_name
+        ,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_N
+    FROM #Student_Name
    )
 ,SocialSecurity
- AS
-	(
-	 SELECT  
-		  CONCAT(
-				   LEFT(ABS(CAST(CAST(NEWID() AS varbinary) AS int)), 3) 
-				 , '-'
-				 , SUBSTRING(CAST(ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Char),3, 2)
-				 , '-'
-				 , SUBSTRING(CAST(ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Char),4, 4)) AS Social_Security
-		 ,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_S
-	 FROM #Student_Name
-	)
+AS
+  (
+   SELECT  
+	CONCAT(LEFT(ABS(CAST(CAST(NEWID() AS varbinary) AS int)), 3) 
+	,'-'
+	,SUBSTRING(CAST(ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Char),3, 2)
+        ,'-'
+	,SUBSTRING(CAST(ABS(CAST(CAST(NEWID() AS varbinary) AS int)) AS Char),4, 4)) AS Social_Security
+        ,ROW_NUMBER() OVER(ORDER BY NEWID()) AS Rank_S
+    FROM #Student_Name
+   )
 ,Collection
- AS
-	(
-	 SELECT 
-		  CAST(Student_Code as Char) as Student_Code
-		 ,LOWER(CONCAT(LEFT(First_Name, 1), LEFT(Middle_Name, 1), LEFT(Last_Name, 1), SUBSTRING(CAST(Student_Code as Char),3, 3), RIGHT(Last_Name, 1))) as Student_ID
-		 ,Social_Security
-		 ,First_Name
-		 ,Middle_Name
-		 ,Last_Name
-	 FROM Cleaned_Code as C
-	 INNER JOIN Names as N
-		 ON C.Rank_C = N.Rank_N
-	 INNER JOIN SocialSecurity as S
-		 ON S.Rank_S = N.Rank_N
-	)
+AS
+  (
+   SELECT 
+   	CAST(Student_Code as Char) as Student_Code
+       ,LOWER(CONCAT(LEFT(First_Name, 1), LEFT(Middle_Name, 1), LEFT(Last_Name, 1), SUBSTRING(CAST(Student_Code as Char),3, 3), RIGHT(Last_Name, 1))) as Student_ID
+       ,Social_Security
+       ,First_Name
+       ,Middle_Name
+       ,Last_Name
+   FROM Cleaned_Code as C
+   INNER JOIN Names as N
+	 ON C.Rank_C = N.Rank_N
+   INNER JOIN SocialSecurity as S
+	 ON S.Rank_S = N.Rank_N
+  )
 INSERT INTO Student_Info (Student_Code, Student_Id, First_Name, Middle_Name, Last_Name, Social_Security)
 SELECT Student_Code, Student_Id, First_Name, Middle_Name, Last_Name, Social_Security
 FROM Collection
 ;
 
-Select *
-from Student_Info
+
+SELECT *
+FROM Student_Info
 
 
